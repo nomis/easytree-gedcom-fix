3dd723ef20ad0ca7e7fff3b80dc499e74d43c060
Simon Arlott <sa.me.uk>
2023-12-30 19:40:48 +0000

    Handle source fields that don't exist

--- easytree-gedcom-fix.py
+++ easytree-gedcom-fix.py
@@ -4,16 +4,49 @@ import io
 
 def apply(source, destination):
 	rec = None
+	rec_n = []
 	sour_fix = None
+	sour_even = []
+	sour_note = []
+
+	sour_fields = {
+		"FILN": "File Number",
+		"REGI": "Register",
+		"MEDI": "Media Type",
+		"LOCA": "Location of Source",
+		"INTV": "Interviewee",
+		"INTE": "Interviewer",
+		"VOL": "Volume Number",
+		"PAGE": "Page",
+		"SUBM": "Submitter",
+		"FILE": "File Name",
+	}
 
 	with open(source, "rt") as i:
 		with io.open(destination, "wt", newline="\r\n") as o:
 			for line in i:
-				line = line.split(" ")
+				line = line.split(" ", 2)
 				if line[0] == "0":
 					if sour_fix is not None:
 						o.write(sour_fix)
+						sour_fix = None
+					if sour_even or sour_note:
+						o.write("1 DATA \n")
+					if sour_even:
+						o.write("2 EVEN \n")
+						for l in sour_even:
+							o.write(l)
+						sour_even = []
+					if sour_note:
+						o.write("2 NOTE " + sour_note[0])
+						for note in sour_note[1:]:
+							o.write("3 CONT " + note)
+						sour_note = []
 					rec = line[2].strip()
+					rec_n = []
+				else:
+					rec_n.append(line[1])
+
 				if rec == "SOUR":
 					if line[0] == "1" and line[1] == "TYPE":
 						line[1] = "TITL"
@@ -21,6 +54,13 @@ def apply(source, destination):
 						continue
 					if line[0] == "1" and line[1] == "TITL":
 						sour_fix = None
+					if line[0] == "1" and line[1] in ("DATE", "PLAC"):
+						line[0] = "3"
+						sour_even.append(" ".join(line))
+						continue
+					if line[0] == "1" and line[1] in sour_fields.keys():
+						sour_note.append(f"{sour_fields[line[1]]}: {line[2]}")
+						continue
 				if rec == "INDI" and line[0] == "1"  and line[1] == "ADDR":
 					continue
 				if rec == "FAM" and line[0] == "1"  and line[1] in ("ADDR", "PHON"): # Not supported
@@ -31,6 +71,11 @@ def apply(source, destination):
 					line[1] = "CONT"
 				if rec == "NOTE" and line[0] == "2" and line[1] == "SOUR":
 					line[0] = "1"
+
+				if line[1] == "TEXT" and rec_n[-2:-1] == ["SOUR"]:
+					# Text from the source needs to be part of the source, not in an XREF
+					line[1] = "NOTE"
+
 				line = " ".join(line)
 				o.write(line)
 
